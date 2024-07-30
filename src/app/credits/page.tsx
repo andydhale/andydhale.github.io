a487e4994919edbe4360a24a0f2a033faf0943a6
Andy Hale <andy@haledev.co.uk>
2024-07-30 06:16:24 +0100

    Adjusted font size on the credits page

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -4,8 +4,8 @@ export default function Credits() {
     return (
         <main className="p-24">
             <h1 className="text-4xl mb-8">Credits</h1>
-            <p>This page would not have been possible without the following:</p>
-            <ul className="list-disc mt-8 text-gray-500">
+            <p className="text-xl">This page would not have been possible without the following:</p>
+            <ul className="list-disc mt-8 text-gray-500 text-xl">
                 <CreditItem text="Microsoft Designer (yes all the images are AI generated, I can't draw!)" />
                 <CreditItem text="NextJS" />
                 <CreditItem text="Visual Studio Code" />
