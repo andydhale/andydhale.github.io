4b634e98710787722ddd0b536e5ac58a1cfacfee
Andy Hale <andy@haledev.co.uk>
2024-07-31 22:57:32 +0100

    Adjusted size of latest post component

--- src/app/components/LatestPost.tsx
+++ src/app/components/LatestPost.tsx
@@ -5,9 +5,11 @@ export default function LatestPost() {
     const post = getPosts(1, 1)[0];
 
     return (
-        <div className="px-24 py-8 max-sm:px-8">
-            <h2 className="text-2xl text-[#FE7E0B] text-stroke-1">Latest Post</h2>
-            <Link className="text-xl" href={`/blog/episode/${post.slug}`}><h4>{ post.title }</h4></Link>
+        <div className="px-24 pt-16 mb-8 max-sm:px-8 flex items-center">
+            <div>
+                <h2 className="text-4xl text-[#FE7E0B] text-stroke-1">Latest Post</h2>
+                <Link className="text-2xl" href={`/blog/episode/${post.slug}`}><h4>{ post.title }</h4></Link>
+            </div>
         </div>
     )
 }
