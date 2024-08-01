189bf773d7c23536d73700860f30d6eac88f10ce
Andy Hale <andy@haledev.co.uk>
2024-08-01 13:49:42 +0100

    Cleaned up files, removed comments and switched homepage images to use Next image component

--- src/app/blog/episode/[slug]/page.tsx
+++ src/app/blog/episode/[slug]/page.tsx
@@ -3,14 +3,10 @@ import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 import markdownStyles from './markdown.module.css';
-import markdownToHtml from "@/app/lib/markdown";
 
 import { Open_Sans } from "next/font/google";
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
-// import remarkClasses from "remark-classes";
-import remarkHtml from "remark-html";
-// import * as remarkCustomBlocks from "remark-custom-blocks";
 import remarkDirective from 'remark-directive';
 import remarkDirectiveRehype from "remark-directive-rehype";
 import { readingTime } from "@/app/lib/readingTime";
@@ -22,40 +18,11 @@ const defaultFont = Open_Sans({
   });
   
 
-// interface IBlogPostParams {
-//     slug: string;
-// }
 type Params = {
     params: {
       slug: string;
     };
   };
-
-  // function myRemarkPlugin() {
-  //   /**
-  //    * @param {import('mdast').Root} tree
-  //    *   Tree.
-  //    * @returns {undefined}
-  //    *   Nothing.
-  //    */
-  //   return (tree) => {
-  //     visit(tree, (node) => {
-  //       if (
-  //         node.type === 'containerDirective' ||
-  //         node.type === 'leafDirective' ||
-  //         node.type === 'textDirective'
-  //       ) {
-  //         if (node.name !== 'note') return
-  
-  //         const data = node.data || (node.data = {})
-  //         const tagName = node.type === 'textDirective' ? 'span' : 'div'
-  
-  //         data.hName = tagName
-  //         data.hProperties = h(tagName, node.attributes || {}).properties
-  //       }
-  //     })
-  //   }
-  // }
 
 export default async function BlogPage({params}: Params) {
     // console.log(`Slug ${slug}`)
@@ -65,13 +32,12 @@ export default async function BlogPage({params}: Params) {
         return notFound();
       }
     
-    const content = await markdownToHtml(post.content || "");
+    //const content = await markdownToHtml(post.content || "");
 
     return (
         <article className="p-24 max-sm:px-8 max-sm:py-12">
             <h1 className="text-4xl mb-16">{post.title}</h1>
             {/* <p>Reading time: { readingTime(content) }</p> */}
-            {/* <div className={`${markdownStyles["markdown"]} ${defaultFont.className}`} dangerouslySetInnerHTML={{ __html: content }}></div> */}
             <ReactMarkdown remarkPlugins={[remarkDirective, remarkDirectiveRehype]} className={`${markdownStyles["markdown"]} ${defaultFont.className} mb-16`}>
               {post.content}
             </ReactMarkdown>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,14 @@
+import Image from "next/image";
+
 import Connect from "./components/Connect";
 import LatestPost from "./components/LatestPost";
 
 export default function Home() {
   return (
-    <main className="">
-      <img src="/images/comic.jpg" />
-      {/* <img src="/images/page_title.jpg" className="absolute site-header" /> */}
+    <main>
+      <Image src="/images/comic.jpg" width={1920} height={2716} alt="Comic Home Page" />
       <LatestPost />
-      {/* <div className="w-screen overflow-x-hidden">
-        <img src="/images/villain_01.png" className="top-[1200px] left-[-200px] absolute transition-all w-[900px]" />
-        <img src="/images/villain_02.png" className="top-[1500px] right-0 absolute  transition-all w-[900px]" />
-      </div> */}
-      {/* <div className="absolute top-[120%] w-[30%] h-[15%] bg-contain left-[50%] translate-x-[-50%] flex justify-center items-center">
-        <img src="/images/speech.png" className="absolute" />
-        <p className="text-center lg:text-2xl max-md:text-md absolute p-24">“Not many SME business's have someone with Andy's technical ability...”</p>
-      </div> */}
-      <img src="/images/page_1.jpg" />
+      <Image src="/images/page_1.jpg" width={1920} height={2716} alt="Comic Page 1" />
       <Connect />
     </main>
   );
